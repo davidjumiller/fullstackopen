e8b0a20648523c19d389706fe607833590844e16
davidjumiller <davidjumiller@gmail.com>
2022-07-09 10:17:59 -0700

    phonebook now uses axios to get data from dummy server (2.11)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,44 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [personsToShow, setPersonsToShow] = useState([...persons])
   const [filterValue, setFilterValue] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      updatePersons(response.data, filterValue)
+    })
+  }, [])
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    
     let nameOccurences = persons.reduce((prev, curr) => {
       let match = curr.name === newName
       return prev + match
     }, 0)
+
     if (nameOccurences > 0) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      const newPersons = persons.concat({ name: newName, number: newNumber })
-      setPersons(newPersons)
+      const newPersons = persons.concat({ name: newName, number: newNumber, id: persons.length+1 })
+      updatePersons(newPersons, filterValue)
       setNewName('')
       setNewNumber('')
-      setPersonsToShow(newPersons.filter((person) => person.name.toLowerCase().includes(filterValue.toLowerCase())))
     }
+  }
+
+  const updatePersons = (persons, filter) => {
+    setPersons(persons)
+    setPersonsToShow(persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase())))
   }
 
   const handleFilterChange = (event) => {
     setFilterValue(event.target.value)
-    setPersonsToShow(persons.filter((person) => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
+    updatePersons(persons, event.target.value)
   }
 
   return (
